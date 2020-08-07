aa551c6951ae6d1c6c01e7c97324bead756fcc66
MaharshiYeluri01 <40265954+MaharshiYeluri01@users.noreply.github.com>
2020-08-07 18:18:05 +0530

    Update radam.py

--- cifar_imagenet/utils/radam.py
+++ cifar_imagenet/utils/radam.py
@@ -146,7 +146,7 @@ class DRAdam(Optimizer):
 #                         step_size=rt*lt*dfc
 #                         state['previous_grad'] = p_data_fp32.clone()
                         rt = math.sqrt( (N_sma - 4) / (N_sma_max - 4) * (N_sma - 2) / N_sma * N_sma_max / (N_sma_max - 2)) 
-                        lt=group['lr'] * math.sqrt((1 - beta2_t)/(1 - beta1 ** state['step'])
+                        lt=group['lr'] * math.sqrt((1 - beta2_t)/(1 - beta1 ** state['step']))
                         dfc=1
                         if rt>=0.99:
                             diff = torch.abs(previous_grad - p_data_fp32)
